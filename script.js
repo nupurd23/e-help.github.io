1e42a96eedd62f8f7a4d3419c9e182ef5e2a3eb3
Nupur <114854035+nupurd23@users.noreply.github.com>
2023-09-24 19:07:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // let a = document.getElementById("video").controls=false;
+document.getElementById("submit").onclick = function(){
+    if($('#phn').val()!='' && $('#name').val()!=' ' && $('#email').val()!=''  && $('#dob').val()!=''){
+        window.location.href="success.html";
+        
+      } else{
+        alert("You have not entered required information");
+      }
+
+}
